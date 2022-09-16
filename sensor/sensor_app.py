b53ac95bc7299cc7c93a28e6d9bd7936ff6595cf
martynas-rakickis <martynas.rakickis@telia.lt>
2022-09-16 16:08:27 +0300

    Module 1 code

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -9,7 +9,9 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
-
+from load_data import load_sensor_data
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
